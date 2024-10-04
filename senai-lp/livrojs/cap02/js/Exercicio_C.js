7ea2102f0cf35c600b7861eef99b699cfe0631a7
Sterneves <sternevesneves@gmail.com>
2024-10-04 19:48:42 -0300

    Exercicio c

--- senai-lp/livrojs/cap02/js/Exercicio_C.js
+++ senai-lp/livrojs/cap02/js/Exercicio_C.js
@@ -3,10 +3,10 @@ const resp1 = document.querySelector("#outResp1")
 const resp2 = document.querySelector("#outResp2")
 
 frm.addEventListener("submit",(e) =>{
-    const produto= (frm.inProduto.value);
+    const produto= frm.inProduto.value
     const valor = Number(frm.inValor.value);
 
-    const total =(( valor * 2));
+    const total =(( valor * 2) + valor * 0.5);
    
 
     resp1.innerText = `Produto : ${produto.toFixed(2)}`
