04bac0dfaca1b31b988205d7924ad6aedee0aa74
Sterneves <sternevesneves@gmail.com>
2024-10-03 21:43:11 -0300

    index

--- senai-lp/livrojs/cap02/js/Exercicio_C.js
+++ senai-lp/livrojs/cap02/js/Exercicio_C.js
@@ -1,14 +1,16 @@
 const frm = document.querySelector("form")
 const resp1 = document.querySelector("#outResp1")
+const resp2 = document.querySelector("#outResp2")
 
 frm.addEventListener("submit",(e) =>{
-    const produto=(frm.inProduto,value)
-    const valor = Number(frm.inValor.value)
+    const produto= (frm.inProduto.value);
+    const valor = Number(frm.inValor.value);
 
-    const total =(( valor * 2) + valor * 0.5);
+    const total =(( valor * 2));
    
 
-    resp1.innerText = `Total R$: ${total.toFixed(2)}`
+    resp1.innerText = `Produto : ${produto.toFixed(2)}`
+    resp2.innerText = `Total R$: ${total.toFixed(2)}`
 
 
     e.preventDefault()
